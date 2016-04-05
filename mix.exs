2179d4283ee95ed3e328287ce5490fa6fc569248
sohjiro <felipe@makingdevs.com>
2016-04-04 19:35:38 -0500

    adding poison lib

--- mix.exs
+++ mix.exs
@@ -27,6 +27,9 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.8.2"}]
+    [
+      {:httpoison, "~> 0.8.2"},
+      {:poison, "~> 2.0"}
+    ]
   end
 end
