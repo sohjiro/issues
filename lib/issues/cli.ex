65c8f4d606a65d762098072a9d3cb69149e17e74
sohjiro <felipe@makingdevs.com>
2016-04-04 21:46:55 -0500

    refactor of the first approach... this guys of prag prog are sick

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,6 +2,8 @@ defmodule Issues.CLI do
 
   @default_count 4
 
+  import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
+
   @moduledoc """
   Handle the command line parsing and the dispatch to
   the various functions that end up generating a
@@ -42,7 +44,7 @@ defmodule Issues.CLI do
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
     |> Enum.take(count)
-    |> print_issues_table
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def process(:help) do
@@ -75,27 +77,47 @@ defmodule Issues.CLI do
     end)
   end
 
-  def print_issues_table(list) do
-    list
-    |> print_headers
-    |> print_body
+  def print_table_for_columns(rows, headers) do
+    with data_by_columns = split_into_columns(rows, headers),
+         column_widths   = widths_of(data_by_columns),
+         format          = format_for(column_widths)
+    do
+      puts_one_line_in_columns(headers, format)
+      IO.puts(separator(column_widths))
+      puts_in_columns(data_by_columns, format)
+    end
   end
 
-  def print_headers(list_of_issues) do
-    ["#", "created_at", "title"]
-    |> Enum.join("\t")
-    |> IO.puts
-
-    list_of_issues
+  def split_into_columns(rows, headers) do
+    for header <- headers do
+      for row <- rows, do: printable(row[header])
+    end
   end
 
-  def print_body([]), do: :done
+  def printable(str) when is_binary(str), do: str
+  def printable(str), do: to_string(str)
 
-  def print_body([%{"number" => number, "created_at" => created_at, "title" => title} | t]) do
-    [number, created_at, title]
-    |> Enum.join("\t")
-    |> IO.puts
-    print_body(t)
+  def widths_of(columns) do
+    for column <- columns, do: column |> map(&String.length/1) |> max
+  end
+
+  def format_for(column_widths) do
+    map_join(column_widths, " / ", fn(width) -> "~-#{width}s" end) <> "~n"
+  end
+
+  def separator(column_widths) do
+    map_join(column_widths, "-+-", fn(width) -> List.duplicate("-", width) end)
+  end
+
+  def puts_in_columns(data_by_columns, format) do
+    data_by_columns
+    |> List.zip
+    |> map(&Tuple.to_list/1)
+    |> each(&puts_one_line_in_columns(&1, format))
+  end
+
+  def puts_one_line_in_columns(fields, format) do
+    :io.format(format, fields)
   end
 
 end
