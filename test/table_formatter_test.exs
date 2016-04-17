0b3a442b2c7224a3695a5d66de7014cb62bb3f73
sohjiro <felipe@makingdevs.com>
2016-04-17 13:01:59 -0500

    adding trailing spaces for test

--- test/table_formatter_test.exs
+++ test/table_formatter_test.exs
@@ -31,5 +31,19 @@ defmodule TableFormatterTest do
     assert TF.format_for([9, 10, 11]) == "~-9s / ~-10s / ~-11s~n"
   end
 
+  test "Output is correct" do
+    result = capture_io fn ->
+      TF.print_table_for_columns(simple_test_data, headers)
+    end
+    assert result == """
+    c1    / c2     / c4     
+    ------+--------+--------
+    r1 c1 / r1 c2  / r1+++c4
+    r2 c1 / r2 c2  / r2 c4  
+    r3 c1 / r3 c2  / r3 c4  
+    r4 c1 / r4++c2 / r4 c4  
+    """
+  end
+
 
 end
