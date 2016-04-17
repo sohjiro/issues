b5fd6c98f7f9be2306befae3e8d1af11aaf69b2b
sohjiro <felipe@makingdevs.com>
2016-04-17 16:32:21 -0500

    adding documentation and change diagonal for a pipe

--- test/table_formatter_test.exs
+++ test/table_formatter_test.exs
@@ -28,7 +28,7 @@ defmodule TableFormatterTest do
   end
 
   test "correct format string returned" do
-    assert TF.format_for([9, 10, 11]) == "~-9s / ~-10s / ~-11s~n"
+    assert TF.format_for([9, 10, 11]) == "~-9s | ~-10s | ~-11s~n"
   end
 
   test "Output is correct" do
@@ -36,12 +36,12 @@ defmodule TableFormatterTest do
       TF.print_table_for_columns(simple_test_data, headers)
     end
     assert result == """
-    c1    / c2     / c4     
+    c1    | c2     | c4     
     ------+--------+--------
-    r1 c1 / r1 c2  / r1+++c4
-    r2 c1 / r2 c2  / r2 c4  
-    r3 c1 / r3 c2  / r3 c4  
-    r4 c1 / r4++c2 / r4 c4  
+    r1 c1 | r1 c2  | r1+++c4
+    r2 c1 | r2 c2  | r2 c4  
+    r3 c1 | r3 c2  | r3 c4  
+    r4 c1 | r4++c2 | r4 c4  
     """
   end
 
