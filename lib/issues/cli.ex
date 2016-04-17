c5192573d2b8cb352c6faa0f6a818e59a82b51ac
sohjiro <felipe@makingdevs.com>
2016-04-17 12:38:49 -0500

    moving code to table formatter module

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,8 +2,6 @@ defmodule Issues.CLI do
 
   @default_count 4
 
-  import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
-
   @moduledoc """
   Handle the command line parsing and the dispatch to
   the various functions that end up generating a
@@ -44,7 +42,7 @@ defmodule Issues.CLI do
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
     |> Enum.take(count)
-    |> print_table_for_columns(["number", "created_at", "title"])
+    |> Issues.TableFormatter.print_table_for_columns(["number", "created_at", "title"])
   end
 
   def process(:help) do
@@ -75,49 +73,6 @@ defmodule Issues.CLI do
     Enum.sort(list_of_issues, fn(i1, i2) ->
       i1["created_at"] <= i2["created_at"]
     end)
-  end
-
-  def print_table_for_columns(rows, headers) do
-    with data_by_columns = split_into_columns(rows, headers),
-         column_widths   = widths_of(data_by_columns),
-         format          = format_for(column_widths)
-    do
-      puts_one_line_in_columns(headers, format)
-      IO.puts(separator(column_widths))
-      puts_in_columns(data_by_columns, format)
-    end
-  end
-
-  def split_into_columns(rows, headers) do
-    for header <- headers do
-      for row <- rows, do: printable(row[header])
-    end
-  end
-
-  def printable(str) when is_binary(str), do: str
-  def printable(str), do: to_string(str)
-
-  def widths_of(columns) do
-    for column <- columns, do: column |> map(&String.length/1) |> max
-  end
-
-  def format_for(column_widths) do
-    map_join(column_widths, " / ", fn(width) -> "~-#{width}s" end) <> "~n"
-  end
-
-  def separator(column_widths) do
-    map_join(column_widths, "-+-", fn(width) -> List.duplicate("-", width) end)
-  end
-
-  def puts_in_columns(data_by_columns, format) do
-    data_by_columns
-    |> List.zip
-    |> map(&Tuple.to_list/1)
-    |> each(&puts_one_line_in_columns(&1, format))
-  end
-
-  def puts_one_line_in_columns(fields, format) do
-    :io.format(format, fields)
   end
 
 end
